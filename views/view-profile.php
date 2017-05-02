4f9878f9eefefd53f96ccb75d78db62336713cf0
Vincent Gercke Pedersen <vgercke@gmail.com>
2017-05-02 16:30:59 +0200

    Merge branch 'master' into vincent
    # Conflicts:
    #	index.php

--- views/view-profile.php
+++ views/view-profile.php
@@ -1,1 +1,28 @@
 <!-- Profile -->
+
+<div class="view view-profile">
+	<div class="container">
+
+		<div class="profile-wrapper">
+
+		<h1>Profile</h1>
+
+			<div class="profile-infoBox box">
+
+				<div class="profile-infoBox__text">
+
+					<p>Username: </p>
+					<input type="" name="">
+					<p>Email: </p>
+					<input type="" name="">
+					<p>Password: </p>
+					<input type="" name="">
+
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
+</div>
