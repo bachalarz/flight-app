7c2d7eaf353d6822323fea241829eda282152827
Thomas Gross Rasmussen <thomasgrossrasmussen@gmail.com>
2017-05-02 15:05:40 +0200

    no message

--- views/view-profile.php
+++ views/view-profile.php
@@ -1,16 +1,18 @@
 <!-- Profile -->
 
 <div class="view view-profile container">
-	
-	<h1>profile</h1>
 
 	<div class="profile-wrapper">
 
-	<div class="profile-info">
+		<h1>profile</h1>
+
+		<div class="profile-info">
 
 			<div class="profile-info__text">
 
-
+				<p>Username</p>
+				<p>Email</p>
+				<p>Password</p>
 
 			</div>
 			
