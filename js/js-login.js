0a8cce1b9f6f9ad390dc74647cf4878e63aea67c
Vincent Gercke Pedersen <vgercke@gmail.com>
2017-05-02 22:01:29 +0200

    Tilføjelse af signup + login view
    Styling (desktop + responsive) og funktionalitet af signup og login
    siderne.
    
    Også tilføjelse af nyt logo.

--- js/js-login.js
+++ js/js-login.js
@@ -1,3 +1,29 @@
 /**********************************/
 //		Login JS
 /**********************************/
+
+$(document).on('click', '#btnCreateAccount', function() {
+    $(".view").hide();
+    $(".view-signup").show();
+});
+
+$(document).on('click', '#btnLogin', function() {
+    fnLogin();
+});
+
+function fnLogin() {
+    var sEmail = $('#inpLoginEmail').val();
+    var sPassword = $('#inpLoginPassword').val();
+    for (var i = 0; i < aUsers.length; i++) {
+        if (sEmail == aUsers[i].email && sPassword == aUsers[i].password) {
+            $(".view").hide();
+            $(".view-profile").show();
+        } else {
+            swal(
+                'Oops...',
+                'Something went wrong! Please try again :)',
+                'error'
+            )
+        }
+    }
+}
--- views/view-signup.php
+++ views/view-signup.php
@@ -1,8 +1,10 @@
 <!-- Signup -->
 
 <div class="view view-signup container">
-	<h1>Signup</h1>
-	<input id="inpEmail" class="inpFirst" type="email" name="email">
-    <input id="inpPassword" class="inpLast" type="password" name="password">
-    <button id="btnSignup" class="btnSuccess">SIGNUP</button>
+	<h1>Welcome to Flight App.</h1>
+	<div id="signupContainer">
+	<input id="inpEmail" class="inpFirst" type="email" name="email" placeholder="Email">
+    <input id="inpPassword" class="inpLast" type="password" name="password" placeholder="Password">
+    </div>
+    <button id="btnSignup" class="btnSuccess">Create Account</button>
 </div>
