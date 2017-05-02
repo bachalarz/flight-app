87185257a377c5f3ec763c84b199570f38180d57
Vincent Gercke Pedersen <vgercke@gmail.com>
2017-05-02 15:04:15 +0200

    Signup funktionalitet til main js

--- index.php
+++ index.php
@@ -43,11 +43,7 @@
 
 	</div> <!-- #main-wrapper -->
 
-<<<<<<< HEAD
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-=======
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
->>>>>>> master
 
 	<script src="js/js-main.js"></script>
 
--- js/js-signup.js
+++ js/js-signup.js
@@ -2,19 +2,4 @@
 //		Signup JS
 /**********************************/
 
-$(document).on("click", "#btnSignup", function() {
-    fnSignup();
-});
 
-var aUsers = [];
-
-function fnSignup() {
-    //Grab the values
-    var sEmail = $('#inpEmail').val();
-    var sPassword = $('#inpPassword').val();
-    if (sEmail && sPassword) {
-        //Push to the array
-        aUsers.push({ "email": sEmail, "password": sPassword });
-        console.log(aUsers);
-    }
-}
