11b8dac413f1a4f94cb92086b2336f5bc4c5290d
Vincent Gercke Pedersen <vgercke@gmail.com>
2017-05-02 16:26:22 +0200

    Primær styling af buttons og inputs + tilføjelse af logo

--- views/view-header.php
+++ views/view-header.php
@@ -6,6 +6,6 @@
 			<i class="fa fa-user" aria-hidden="true"></i>
 			<i class="fa fa-search" aria-hidden="true"></i>
 		</div>
-		<img src="img/logo.png">
+		<img src="img/logo.svg">
 	</div>
 </header>
