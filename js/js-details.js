def8884780fab89df316263051591330c63c2ed6
Lomborg <d.lomborg@hotmail.com>
2017-05-04 11:30:24 +0200

    no message

--- js/js-details.js
+++ js/js-details.js
@@ -3,3 +3,9 @@
 /**********************************/
 
 console.log("details");
+
+
+$(document).on('click', '#btn-details', function() {
+    $(".view").hide();
+    $(".view-confirmation").show();
+});	
--- views/view-details.php
+++ views/view-details.php
@@ -4,6 +4,32 @@
 
 <h1>Details</h1>
 
+	<div id="details-container ">
+		<div class="ticket-type form-control">Ticket type: <br>
+		    <input type="radio" name="gender" value="male" checked> Economy<br>
+			<input type="radio" name="gender" value="female"> Premium Economy ( +30£ )<br>
+			<input type="radio" name="gender" value="other"> Business class ( +100£ )<br> 
+			<input type="radio" name="gender" value="female"> First class ( +250£ )
+		</div>
+
+	<div class="Baggages form-control">
+		Baggages: 
+    	<select name="Baggages">
+	    <option value="1">1</option>
+	    <option value="2">2</option>
+	    <option value="3">3</option>
+	    <option value="5">5</option>
+	    <option value="6">6</option>
+	    <option value="7">7</option>
+  </select>
+      </div>
+
+
+    <button id="btn-details" class="btnSuccess">Accept details</button>
+
+    </div>
+	
+
 	<?php
 		require "view-flow.php";
 	?>
