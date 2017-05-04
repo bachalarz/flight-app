027d5babcadc17334326975291d793e8d9d1c55a
Lomborg <d.lomborg@hotmail.com>
2017-05-04 11:46:22 +0200

    no message

--- js/js-details.js
+++ js/js-details.js
@@ -1,10 +1,6 @@
 /**********************************/
 //		Details JS
 /**********************************/
-
-console.log("details");
-
-
 $(document).on('click', '#btn-details', function() {
     $(".view").hide();
     $(".view-confirmation").show();
